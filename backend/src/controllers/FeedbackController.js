418bf0b9fb323648fb2bb1168a61d866aed95a88
pedro-limma <pedro.lima.wk@gmail.com>
2020-07-21 21:12:03 -0300

    Funções finalizadas do feedback

--- backend/src/controllers/FeedbackController.js
+++ backend/src/controllers/FeedbackController.js
@@ -1,14 +1,43 @@
 const connection = require('../database/connection');
 
-module.exports ={
+module.exports = {
+  async index(request, response){
+    const patient_cpf = request.headers.authorization;
+
+    const feedback = await connection('feedback')
+    .where('patient_cpf', patient_cpf)
+    .select('*');
+
+    return response.json(feedback);
+  },
+
   async create(request, response){
     const { feedback } = request.body;
 
     const patient_cpf = request.headers.authorization;
 
     const [id] = await connection('feedback').insert({
-      feedback
+      feedback,
+      patient_cpf
     });
+
+    return response.status(204).send();
+  },
+
+  async delete(request, response){
+    const { id } = resquest.params;
+    const patient_cpf = request.headers.authorization;
+
+    const feedback = await connection('feedback')
+      .where('id', id)
+      .select('patient_cpf')
+      .first();
+
+    if(feedback.patient_cpf !== patient_cpf){
+      return response.status(401).json({ error: 'Operation not permitted' });
+    }
+
+    await connection('feedback').where('id', id).delete();
 
     return response.status(204).send();
   }
--- backend/src/routes.js
+++ backend/src/routes.js
@@ -5,11 +5,15 @@ const routes = express.Router();
 const StudentController = require('./controllers/StudentController');
 const SessionController = require('./controllers/SessionController');
 const PatientController = require('./controllers/PatientController');
+const FeedbackController = require('./controllers/FeedbackController');
 
 routes.post('/session', SessionController.create);
 routes.post('/students', StudentController.create);
 routes.post('/patients/create', PatientController.create);
 routes.get('/patients/index', PatientController.index);
+routes.post('/feedback/create', FeedbackController.create);
+routes.get('/feedback/index', FeedbackController.index);
+routes.delete('/feedback/delete/:id', FeedbackController.delete);
 
 
 
